5b7118ddf9e1b537fc323dbeab663ac43bb1595d
Joe Elsey <joe.elsey@gmail.com>
2014-11-13 11:51:56 -0800

    added root msg

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.listen(app.get('port'), function() {
   console.log('server running on port: ' + app.get('port'));
 });
 
+app.get('/', function(req, res){
+  res.send('Add your state and city to the end of the url in the following format:  "/state initials/city name"\n
+    If entered correctly you should see the temp for where you live.');
+})
+
 app.get('/:state/:city', function(req, res) {
   var key = 'b40a0fca31859481'
   var weatherUrl = 'http://api.wunderground.com/api/' + key +
