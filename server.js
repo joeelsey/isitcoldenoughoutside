b079c82f5dcd20b71d775748e537e9d96af6bb4a
Joe Elsey <joe.elsey@gmail.com>
2014-11-13 17:58:07 -0800

    finds by zip

--- server.js
+++ server.js
@@ -9,21 +9,30 @@ app.listen(app.get('port'), function() {
   console.log('server running on port: ' + app.get('port'));
 });
 
+//app.set('views', __dirname + './');
+//app.set('view engine', 'html');
+
 app.get('/', function(req, res){
-  res.json({msg:'add state and city to the end of the url /state/city to get temp'});
+  res.json({msg:'add zip code to the end of the url /zip to get temp'});
 });
 
-app.get('/:state/:city', function(req, res) {
+app.get('/:zip', function(req, res) {
   var key = 'b40a0fca31859481'
   var weatherUrl = 'http://api.wunderground.com/api/' + key +
-                    '/conditions/q/' + req.params.state + "/" + req.params.city + '.json';
+                    '/conditions/q/' + req.params.zip + '.json';
 
   request
     .get(weatherUrl)
     .end(function(err, weatherdata) {
       if (err) res.status(500).send(err);
       var parsedData = JSON.parse(weatherdata.text);
-      res.json({'temp in your city': parsedData.current_observation.temperature_string});
+      var temp = parsedData.current_observation.temp_f;
+      if(temp < 50 ){
+        res.json({'temp in your city': temp + ' and its cold enough to store beer outside.'});
+      } else {
+        res.json({'temp in your city': temp + ' and its too warm to store beer outside.'})
+      }
+      
     });
 });
 
