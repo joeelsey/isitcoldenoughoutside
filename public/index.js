e3f8b56de8ed1d8f7378c10260ba4494f02abbf7
Joe Elsey <joe.elsey@gmail.com>
2014-11-14 20:14:56 -0800

    json

--- public/index.js
+++ public/index.js
@@ -11,7 +11,7 @@ $(document).on('ready', function() {
     $.ajax({
       type: 'GET',
       dataType: 'text',
-      url: 'https://www.wunderground.com/api/' + key + '/conditions/q/' + ZipCode,
+      url: 'https://www.wunderground.com/api/' + key + '/conditions/q/' + ZipCode + '.json',
       success: function(data) {
         $('#answer').html(data);
       }
